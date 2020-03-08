9d0292b9bde500c1f1b64a23983a911be2c71603
manish-dalal <manish.dalal@daffodilsw.com>
2020-03-08 23:28:44 +0530

    Updates

---
+++ precache-manifest.5181d4ce5fedfa93060359d5d7b92d48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71167cc011a5c1c78aeb8f3f16d65045",
+    "url": "/stashaway-frontend/index.html"
+  },
+  {
+    "revision": "4af8bec1613f9bcb6abb",
+    "url": "/stashaway-frontend/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "cdd2b55b49482e63f564",
+    "url": "/stashaway-frontend/static/css/main.bd143ba9.chunk.css"
+  },
+  {
+    "revision": "4af8bec1613f9bcb6abb",
+    "url": "/stashaway-frontend/static/js/2.6111d3a0.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/stashaway-frontend/static/js/2.6111d3a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd2b55b49482e63f564",
+    "url": "/stashaway-frontend/static/js/main.e129adcb.chunk.js"
+  },
+  {
+    "revision": "f41a27a033a78d110b32",
+    "url": "/stashaway-frontend/static/js/runtime-main.22a63bdb.js"
+  },
+  {
+    "revision": "c4750dc093a792411c9a3267df64acd6",
+    "url": "/stashaway-frontend/static/media/defaultBg.c4750dc0.jpeg"
+  }
+]);
